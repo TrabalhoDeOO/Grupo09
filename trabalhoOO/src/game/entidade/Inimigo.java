a6b33ca5054b9dbc8380d095a4adf36fa0a50924
ShutUpPaulo <markes.calado@gmail.com>
2013-10-20 23:15:31 -0200

    abstração de Inimigo

--- trabalhoOO/src/game/entidade/Inimigo.java
+++ trabalhoOO/src/game/entidade/Inimigo.java
@@ -1,16 +1,14 @@
 package game.entidade;
 
 import game.entidade.Loot;
-import java.util.ArrayList;
-/**
- * 
- */
+//import java.util.ArrayList;
+
 
 /**
  * @author Bruno
  *
  */
-public class Inimigo {
+public abstract class Inimigo {
 
 	/**
 	 * 
@@ -24,101 +22,12 @@ public class Inimigo {
 	public int atk;	
 	
 	public Inimigo() {
-		// TODO Auto-generated constructor stub
-		this.nome = "sem nome";
-		this.tipo = "sem tipo";
-		this.spe = ((this.lvl*2)-hp/2);
-		this.atk =0;
-		
-		Loot loot = new Loot(this);
-		this.loot = loot;
-		
 	}
-	
-	public Inimigo(String nomeInimigo, int lvlInimigo,	String tipoInimigo){
 		
+	public abstract Loot getLoot();
 		
-		this.nome = nomeInimigo;
-		this.lvl = lvlInimigo;
-		this.tipo = tipoInimigo;
-		this.spe = ((this.lvl*3)-hp/2);
-		Loot loot = new Loot(this);
-		this.loot = loot;
+	public abstract String getNome();
+		
+	public abstract void setLvl(int lvl);
 		
 	}
-	
-	public Loot getLoot(){
-		return loot;
-	}
-	
-	public void setLoot(Loot loot){
-		this.loot = loot;
-	}
-	
-	public String getNome(){
-		return nome;
-	}
-	
-	public void setNome(String nome){
-		
-		this.nome = nome;
-		
-	}
-	
-	public int getLvl(){
-		return lvl;
-	}
-	
-	public void setLvl(int lvl){
-		this.lvl = lvl;
-	}
-	
-	public String getTipo(){
-		return tipo;
-	}
-	
-	public void setTipo(String tipo){
-		this.tipo = tipo;
-	}
-	
-	public int getHp() {
-		return hp;
-	}
-
-	public void setHp(int hp) {
-		this.hp = hp;
-	}
-
-	public int getSpe() {
-		return spe;
-	}
-
-	public void setSpe(int spe) {
-		this.spe = spe;
-	}
-	public int getAtk() {
-		return atk;
-	}
-
-	public void setAtk(int atk) {
-		this.atk = atk;
-	}
-
-	public void criarLoot(){
-		Loot loot = new Loot(null);
-		setLoot(loot);
-	}
-	
-	
-	 public void listarInimigo(ArrayList<Inimigo> monstro){
-	
-		 // consultar a professora sobre como referenciar o atributo defesa, fruto de uma sub classe
-		for(int i = 0; i < monstro.size(); i++){
-			System.out.println(monstro.get(i).nome + " " +  monstro.get(i).tipo + " lvl " + monstro.get(i).lvl + " hp: " + monstro.get(i).hp + " atk: " + monstro.get(i).atk
-					+/** " def: " + monstro.get(i).def + **/" spe: " + monstro.get(i).spe);
-			
-		}
-	
-	}
-	
-}
--- trabalhoOO/src/game/entidade/InimigoEvento.java
+++ trabalhoOO/src/game/entidade/InimigoEvento.java
@@ -1,5 +1,8 @@
 package game.entidade;
 
+import java.util.ArrayList;
+
+
 // Trabalhar a questão do ArrayList depois
 public class InimigoEvento extends Inimigo{
 	public int def;
@@ -7,17 +10,29 @@ public class InimigoEvento extends Inimigo{
 	
 	public InimigoEvento() {
 		super();
+		this.nome = "sem nome";
+		this.tipo = "sem tipo";
+		this.spe = ((this.lvl*2)-hp/2);
+		this.atk =0;
 		this.lvl = 0;
-		this.atk =  0;
 		this.def = 0;
 		this.hp = 0;
+		
+		Loot loot = new Loot(this);
+		this.loot = loot;
+				
 		}
 	//, int atkI, int defI, int hpI
 	public InimigoEvento(String nomeInimigo, int lvlInimigo, String tipoInimigo){
-		super(nomeInimigo,lvlInimigo,tipoInimigo);
 		this.atk =   (this.lvl*4);
 		this.def = (this.lvl*2);
 		this.hp = (this.atk/2+this.def*2);
+		this.nome = nomeInimigo;
+		this.lvl = lvlInimigo;
+		this.tipo = tipoInimigo;
+		this.spe = ((this.lvl*3)-hp/2);
+		Loot loot = new Loot(this);
+		this.loot = loot;
 		}
 	
 	public int getDef() {
@@ -27,4 +42,72 @@ public class InimigoEvento extends Inimigo{
 	public void setDef(int def) {
 		this.def = def;
 	}
+	public Loot getLoot(){
+		return loot;
+	}
+	
+	public void setLoot(Loot loot){
+		this.loot = loot;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public void setNome(String nome){
+		
+		this.nome = nome;
+		
+	}
+	
+	public int getLvl(){
+		return lvl;
+	}
+	
+	public void setLvl(int lvl){
+		this.lvl = lvl;
+	}
+	
+	public String getTipo(){
+		return tipo;
+	}
+	
+	public void setTipo(String tipo){
+		this.tipo = tipo;
+	}
+	
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getSpe() {
+		return spe;
+	}
+
+	public void setSpe(int spe) {
+		this.spe = spe;
+	}
+	public int getAtk() {
+		return atk;
+	}
+
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+
+	public void criarLoot(){
+		Loot loot = new Loot(null);
+		setLoot(loot);
+			
+	for(int i = 0; i < monstro.size(); i++){
+		System.out.println(monstro.get(i).nome + " " +  monstro.get(i).tipo + " lvl " + monstro.get(i).lvl + " hp: " + monstro.get(i).hp + " atk: " + monstro.get(i).atk
+				+ " def: " + monstro.get(i).def + " spe: " + monstro.get(i).spe);
+		
+	}
+
+}
 }
