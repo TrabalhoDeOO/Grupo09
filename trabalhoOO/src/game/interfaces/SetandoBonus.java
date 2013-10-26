5a34c7538a717feb43be1bc67a2db5be5afac0c9
Markes <markes.calado@gmail.com>
2013-10-26 01:15:06 -0200

    Criação da interface SetandoBonus

---
+++ trabalhoOO/src/game/interfaces/SetandoBonus.java
@@ -0,0 +1,12 @@
+package game.interfaces;
+
+import game.entidade.*;
+
+public interface SetandoBonus {
+	
+	public void setHp (int hp);
+	public void setAtk (int atk);
+	public void setDef (int def);
+	public void verificarItem (Item item);
+
+}
