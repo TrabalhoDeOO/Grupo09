ebc4b06c4534cf443ab3d1fd601d2b80db321c7c
DaniloBarros <dan.b412@gmail.com>
2013-11-07 16:00:42 -0200

    Arrumado descricao de loja; Adicionado JUnit de loja

---
+++ trabalhoOO/src/game/teste/LojaTest.java
@@ -0,0 +1,51 @@
+package game.teste;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import game.entidade.Consumivel;
+import game.entidade.Item;
+import game.entidade.Loja;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LojaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetItens() {
+		Loja lojinha = new Loja("Chef", "Comidas");
+		Consumivel comida = new Consumivel("Bolo", 100, 5);
+		Consumivel comida2 = new Consumivel("Pao", 101, 2);
+		Consumivel comida3 = new Consumivel("Torta", 102, 6);
+		Consumivel comida4 = new Consumivel("Rocambole", 103, 6);
+		Consumivel comida5 = new Consumivel("Pastel", 104, 7);
+		ArrayList<Item> itens = new ArrayList<Item>();
+		itens.add(comida);
+		itens.add(comida2);
+		itens.add(comida3);
+		itens.add(comida4);
+		itens.add(comida5);
+		lojinha.adicionaItem(comida);
+		lojinha.adicionaItem(comida2);
+		lojinha.adicionaItem(comida3);
+		lojinha.adicionaItem(comida4);
+		lojinha.adicionaItem(comida5);
+		lojinha.criarDescricao("lojinha da hora");
+		lojinha.listarItens();
+		itens.remove(4);
+		assertEquals(lojinha.getItens(),itens);
+	}
+	
+	
+}
