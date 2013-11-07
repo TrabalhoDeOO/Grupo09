8098e4b3c2a82d66f3ba7738f214389cac5f6961
Markes <markes.calado@gmail.com>
2013-11-07 21:22:41 -0200

    conversão da classe MainGame em Window e criação da classe Game -PS.: importantes alterações ainda serão feitas em ambas

---
+++ trabalhoOO/src/game/Game.java
@@ -0,0 +1,24 @@
+package game;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 512;
+	public static final int HEIGTH = WIDTH / 12 * 9;
+	public static final int SCALE = 2;
+	public static final String TITLE = "Are you a neanderthal?";
+	
+	public void run(){
+		
+	}
+	public static void main(String args[]) {
+		// TODO Auto-generated constructor stub
+			
+	}
+
+}
