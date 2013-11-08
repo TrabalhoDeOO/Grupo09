09fc1e4d6361d8cfc81b022158d2e8b063df17f0
Markes <markes.calado@gmail.com>
2013-11-08 02:24:07 -0200

    Ajustes em Game e Window, correção do Loop - tela preta

--- trabalhoOO/src/game/Game.java
+++ trabalhoOO/src/game/Game.java
@@ -1,82 +1,91 @@
 package game;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 public class Game extends Canvas implements Runnable {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1;
 	public static final int WIDTH = 512;
 	public static final int HEIGTH = WIDTH / 12 * 9;
 	public static final int SCALE = 2;
 	public static final String TITLE = "Are you a neanderthal?";
 	
-	private int fps;
-	private int ticks;
+	private BufferedImage spriteSheet = null;
 	private boolean running = false;
 	private Thread thread;
 	
 	public synchronized void start(){
+		if (running)
+			return;
+			
+			running = true;
+			thread = new Thread(this);
+			thread.start();
 }
 	public void run(){
 		long lastTime = System.nanoTime();
-		final double amountOfTicks = 60.0;
+		double amountOfTicks = 60.0;
 		double ns = 1000000000 / amountOfTicks;
 		double delta = 0;
-		int updates =0;
-		int frames = 0;
 		long timer = System.currentTimeMillis();
-		
-		while (running){
+		int updates = 0;
+		int frames = 0;
+		while(running){
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
-			while (delta >= 1){
+			while(delta >= 1){
 				tick();
 				updates++;
 				delta--;
 			}
 			render();
 			frames++;
-		
-		if (System.currentTimeMillis() - timer > 1000){
-			timer += 1000;
-			int fps = frames;
-			int ticks = updates;
-			System.out.println("FPS: " + frames + "TICKS: " + updates);
-			updates = 0;
-			frames = 0;
-		}
+					
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: " + frames + " TICKS: " + updates);
+				frames = 0;
+				updates = 0;
+			}
 		}
 		stop();
 		}
 	
 	
 	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null){
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		//////////////////////////////////
+		g.setColor(Color.black);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		/////////////////////////////////
+		g.dispose();
+		bs.show();
 	}
 	
+	
 	private void tick() {
+		
 	}
 	
 	public synchronized void stop(){
 	}
 	
 	public static void main(String args[]) {
-		// TODO Auto-generated constructor stub
 		new Window (WIDTH, HEIGTH, TITLE, new Game());
 			
 	}
-	public int getTicks() {
-		return ticks;
-	}
-	public void setTicks(int ticks) {
-		this.ticks = ticks;
-	}
-	public int getFps() {
-		return fps;
-	}
-	public void setFps(int fps) {
-		this.fps = fps;
-	}
+	
 	public Thread getThread() {
 		return thread;
 	}
--- trabalhoOO/src/game/Window.java
+++ trabalhoOO/src/game/Window.java
@@ -1,109 +1,10 @@
 package game;
 
-import java.awt.Canvas;
 import java.awt.Dimension;
-//import java.awt.Graphics;
-//import java.awt.image.BufferStrategy;
-//import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
-public class Window extends Canvas implements Runnable {
-	
-	//Dados estáticos
-	//private static final long serialVersionUID = 1L;
-	//public static final int WIDTH = 512;
-//	public static final int HEIGTH = WIDTH / 12 * 9;
-//	public static final int SCALE = 2;
-//	public static final String TITLE = "Are you a neanderthal?";
-	
-/*	private BufferedImage image = new BufferedImage(WIDTH, HEIGTH, BufferedImage.TYPE_INT_RGB);
-	private BufferedImage background = null;
-	private boolean running = false;
-	private Thread thread;
-			
-/*	private synchronized void start(){
-		if (running)
-		return;
-		
-		running = true;
-		thread = new Thread(this);
-		thread.start();
-	}
-	
-	private synchronized void stop(){
-		if (!running)
-			return;
-		
-		running = false;
-		try {
-			thread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.exit(1);
-	}
-	*/
-	public void run(){
-		/*init();
-		long lastTime = System.nanoTime();
-		final double amountOfTicks = 60.0;
-		double ns = 1000000000 / amountOfTicks;
-		double delta = 0;
-		int updates =0;
-		int frames = 0;
-		long timer = System.currentTimeMillis();
-		
-		while (running){
-			long now = System.nanoTime();
-			delta += (now - lastTime) / ns;
-			lastTime = now;
-			if (delta >= 1){
-				tick();
-				updates++;
-				delta--;
-			}
-			render();
-			frames++;
-		
-		if (System.currentTimeMillis() - timer > 1000){
-			timer += 1000;
-			System.out.println(updates+ " Ticks, Fps " + frames);
-			updates = 0;
-			frames = 0;
-		}
-		}
-		stop();*/
-	}
-	
-	
-	//private void init() {
-		// TODO Auto-generated method stub
-		
-//	}
-
-	public void tick() {
-		}
-
-	/*private void render(){
-		BufferStrategy bs = this.getBufferStrategy();
-		if (bs == null){
-			createBufferStrategy(3);
-			return;
-		}
-		
-		Graphics g = bs.getDrawGraphics();
-		////////////////////////////////
-		g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
-		//g.setColor(Color.red);
-		g.fillRect(0, 0, 800, 800);
-		g.drawImage(background, 0,0, null);
-		
-		
-		///////////////////////////////////////
-		g.dispose();
-		bs.show();	
-	}*/
+public class Window{
 	
 	public Window (int WIDTH, int HEIGTH, String TITLE, Game game){
 		
@@ -122,4 +23,7 @@ public class Window extends Canvas implements Runnable {
 		game.start();
 	}
 
+	public static void main(String args[]) {
+		
+	}
 }
